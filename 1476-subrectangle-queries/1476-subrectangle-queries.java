aeee468b5d5efaa67db8087187cfbda5d0cbbf61
stormadi <95018698+stormadi@users.noreply.github.com>
2022-06-22 23:34:34 +0530

    Time: 33 ms (70.77%), Space: 56.4 MB (12.54%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.java
+++ 1476-subrectangle-queries/1476-subrectangle-queries.java
@@ -1,11 +1,11 @@
 class SubrectangleQueries {
     int arr[][];
     public SubrectangleQueries(int[][] rectangle) {
-        arr = rectangle;//new int[rectangle.length][rectangle[0].length];
-        //for(int i = 0; i < rectangle.length; i++)
-        //{
-        //    System.arraycopy(rectangle[i],0,arr[i],0,rectangle[i].length);
-        //}
+        arr = new int[rectangle.length][rectangle[0].length];
+        for(int i = 0; i < rectangle.length; i++)
+        {
+            System.arraycopy(rectangle[i],0,arr[i],0,rectangle[i].length);
+        }
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {

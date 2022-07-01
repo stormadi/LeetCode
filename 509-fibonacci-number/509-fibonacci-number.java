55933c0d9fce405ee362bfb14972177da318394c
stormadi <95018698+stormadi@users.noreply.github.com>
2022-07-02 00:01:25 +0530

    Time: 17 ms (10.83%), Space: 41.3 MB (17.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,23 +1,9 @@
 class Solution 
 {
-    int c;
-    int calcfib(int n, int a, int b)
-    {
-        if(n == 1)
-        {
-            return a + b;
-        }
-        c = a + b;
-        calcfib(n-1,b,c);
-        return c;
-    }
     public int fib(int n)
     {
         if(n == 0) return 0;
-        if(n == 1) return 1;
-        int a = 0;
-        int b = 1;
-        int ans = calcfib(n,a,b);
-        return ans;
+        if(n == 1) return 1;;
+        return fib(n-1) + fib(n-2);
     }
 }

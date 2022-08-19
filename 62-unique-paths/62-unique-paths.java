33a93d830a251a6843bb68757aa2de37d9db1fd9
stormadi <95018698+stormadi@users.noreply.github.com>
2022-08-19 14:45:28 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (68.27%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,27 +1,15 @@
-//DP solution but not optimal
-class Solution 
+//optimal solution
+class Solution
 {
-    public int solve(int i, int j, int m, int n, int[][] matrix)
-    {
-        if(i >= n || j >= m)
-        {
-            return 0;
-        }
-        if(i == n-1 && j == m-1)
-        {
-            return 1;
-        }
-        if(i < n && j < m && matrix[i][j] != 0)
-        {
-            return matrix[i][j];
-        }
-        else return matrix[i][j] = solve(i+1, j, m, n, matrix) + solve(i, j+1, m, n, matrix);
-    }
     public int uniquePaths(int m, int n)
     {
-        int i = 0, j = 0;
-        int[][] mat = new int[n][m];
-        int res = solve(i, j, m, n, mat);
-        return res;
+        int N = n + m - 2;
+        int r = m - 1;
+        double res = 1;
+        for(int i = 1; i <= r; i++)
+        {
+            res = res * (N - r + i)/ i;
+        }
+        return (int)res;
     }
 }

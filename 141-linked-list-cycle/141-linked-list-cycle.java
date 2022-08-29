7d33bf9205dda494ff2b07f8fa4feb722b7f92ea
stormadi <95018698+stormadi@users.noreply.github.com>
2022-08-29 21:21:40 +0530

    Time: 1 ms (45.59%), Space: 46 MB (39.36%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -13,6 +13,7 @@ public class Solution
 {
     public boolean hasCycle(ListNode head)
     {
+        if(head == null || head.next == null) return false;
         ListNode slow = head, fast = head;
         while(fast != null && fast.next != null)
         {
